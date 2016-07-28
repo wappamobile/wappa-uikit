4142ece053cb804a3e57d74e2fa647ccecca803f
Nelson <nelsondelega@gmail.com>
2016-07-28 11:51:39 -0300

    gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,28 @@
-var gulp        = require('gulp');
+'use strict';
+
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+
+var clean = require('gulp-clean');
+var cssmin = require('gulp-cssmin');
+var concatCss = require('gulp-concat-css');
+var stripCssComments = require('gulp-strip-css-comments');
+
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('styles', function () {
+    return gulp.src([
+            'src/bower/bootstrap/dist/css/bootstrap.css',
+            'src/styles/**/*.css',
+            '!src/guideline.css',
+            '!src/docs.css'
+        ])
+        .pipe(concatCss('wappa-uikit.min.css'))
+        .pipe(stripCssComments( { all: true } ))
+        .pipe(cssmin())
+        .pipe(gulp.dest( 'dist' ));
+});
 
 gulp.task('browser-sync', function () {
     browserSync.instance = browserSync.init(
@@ -19,3 +42,5 @@ gulp.task('browser-sync', function () {
 });
 
 gulp.task('serve', ['browser-sync']);
+
+gulp.task('build', ['styles']);
